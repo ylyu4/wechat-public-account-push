db1d7aee909e56b74cea325e847edfb3a9bc642c
ylyu4 <57893326+ylyu4@users.noreply.github.com>
2022-11-30 22:37:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '57d1c2792870c01e6ea6bbd5fceaba6e',
+  CALLBACK_TEMPLATE_ID: '4h9wvrPvrkmrzwx7IO8nPARyXbrF2Qt5dr9vltCrsho',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxf178ceafa5ca09f0',
+      id: 'olzgn6OvCmt5tmuIkfeZgDN67im8',
     }
   ],
 
